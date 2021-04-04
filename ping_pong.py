98bd1eb15c18674687b68e326e6d01f517b35705
darkpydeu <darkpydeu@yandex.ru>
2021-04-04 19:11:33 +0500

    Добавлен мяч

--- ping_pong.py
+++ ping_pong.py
@@ -1,5 +1,4 @@
 from pygame import *
-'''Необходимые классы'''
 
 #класс-родитель для спрайтов 
 class GameSprite(sprite.Sprite):
@@ -26,33 +25,31 @@ class Player(GameSprite):
         keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < win_height - 80:
+        if keys[K_DOWN] and self.rect.y < win_height - 150:
             self.rect.y += self.speed
     def update_l(self):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_s] and self.rect.y < win_height - 80:
+        if keys[K_s] and self.rect.y < win_height - 150:
             self.rect.y += self.speed
 
 #Игровая сцена:
-back = (200, 255, 255) # цвет фона (background)
+back = (200, 255, 255)
 win_width = 600
 win_height = 500
 window = display.set_mode((win_width, win_height))
 window.fill(back)
 
 #флаги отвечающие за состояние игры
-game = True
 finish = False
 clock = time.Clock()
 FPS = 60
 
 #создания мяча и ракетки    
-racket1 = Player('racket.png', 30, 200, 4, 50, 150) # при созданни спрайта добавляется еще два параметра
+racket1 = Player('racket.png', 30, 200, 4, 50, 150)
 racket2 = Player('racket.png', 520, 200, 4, 50, 150)
 ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
-
 font.init()
 font = font.Font(None, 35)
 lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
@@ -61,6 +58,7 @@ lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
 speed_x = 3
 speed_y = 3
 
+game = True
 while game:
     for e in event.get():
         if e.type == QUIT:
@@ -70,32 +68,26 @@ while game:
         window.fill(back)
         racket1.update_l()
         racket2.update_r()
-        ball.rect.x += speed_x
-        ball.rect.y += speed_y
-
+        if ball.rect.y <= 0 or ball.rect.y >= win_height-50:
+            speed_y *= -1
+        elif ball.rect.x <= 0 or ball.rect.x >= win_width - 50:
+            if ball.rect.x <= 0:
+                window.blit(lose1, (250, 250))
+                finish = False
+                break
+            elif ball.rect.x >= win_width - 50:
+                window.blit(lose2, (250, 250))
+                finish = False
+                break
         if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
             speed_x *= -1
-            speed_y *= 1
-        
-        # если мяч достигает границ экрана меняем направление его движения
-        if ball.rect.y > win_height-50 or ball.rect.y < 0:
-            speed_y *= -1
 
-        # если мяч улетел дальше ракетки, выводим условие проигрыша для первого игрока
-        if ball.rect.x < 0:
-            finish = True
-            window.blit(lose1, (200, 200))
-            game_over = True
+        ball.rect.y += speed_y
+        ball.rect.x += speed_x
 
-        # если мяч улетел дальше ракетки, выводим условие проигрыша для второго игрока
-        if ball.rect.x > win_width:
-            finish = True
-            window.blit(lose2, (200, 200))
-            game_over = True
-
+        ball.reset()
         racket1.reset()
         racket2.reset()
-        ball.reset()
 
-    display.update()
-    clock.tick(FPS)
+        display.update()
+        clock.tick(FPS)
